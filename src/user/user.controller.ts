163f83ae7a54c68491e29d92ca1f4e491735f286
pamuDev <e.ariaspelaez@gmail.com>
2024-08-18 18:35:18 -0500

    Endpoints getAllUsers, user/{id}, user/name mañana agregar dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {Controller, Post, Body, Get, Param} from '@nestjs/common';
 import {UserService} from "./user.service";
+import {User} from "./schemas/user.schema";
 
 @Controller('user')
 export class UserController {
 
     constructor(private userService: UserService) {}
 
+    @Get('getAllUsers')
+    async findAll(){
+        return this.userService.findAll();
+    }
+
+    @Get(':_id')
+    async findById(@Param('_id') id:string){
+        return this.userService.findOneById(id);
+    }
+
+    //En caso de referencia
+    @Get('name/:name')
+    async findOneByName(@Param('name') name: string): Promise<User> {
+        return this.userService.findOneByName(name);
+    }
+
+
     @Post()
     async create(@Body() createUser: any){
         return this.userService.create(createUser);
     }
+
+
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import { InjectModel} from "@nestjs/mongoose";
 import {Model} from "mongoose";
 
@@ -10,9 +10,32 @@ export class UserService {
     constructor(
         @InjectModel(User.name) private userModel: Model<User>) {}
 
+
     async create(user:any){
         const createdUser = new this.userModel(user);
         return createdUser.save();
     }
+
+    async findAll(){
+        return this.userModel.find().exec();
+    }
+
+    async findOneById(id: string){
+        const user = await this.userModel.findById(id).exec();
+        if (!user){
+            throw new NotFoundException(`User with ID ${id} not found`);
+        }
+        return user;
+    }
+
+    async findOneByName(name: string){
+        const user = await this.userModel.findOne({name}).exec();
+        if (!user){
+            throw new NotFoundException(`User with name ${name} not found`);
+        }
+        return user;
+    }
+
+
 
 }
